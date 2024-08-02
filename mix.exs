31b23b2ca26183aa81adf34eb51acc65b64510d5
neel vinoth <nealbpm6@gmail.com>
2024-08-01 22:29:09 -0400

    Added Flop and Flop Phoenix dependencies

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule Water.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:flop, "~> 0.25.0"},
+      {:flop_phoenix, "~> 0.22.9"}
     ]
   end
 
